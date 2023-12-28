2b0492a3f984015ed6241e4ece5a256cc53b2319
jhonatan1997ac <jhonatan.alarcon2767@utc.edu.ec>
2023-12-27 20:35:47 -0500

    cambios en catalogo version 1

--- lib/pages/inicio/usabilidad/catalogo.dart
+++ lib/pages/inicio/usabilidad/catalogo.dart
@@ -20,7 +20,7 @@ class Catalogo extends StatelessWidget {
       name: 'Producto 1',
       description: 'Descripción del Producto 1',
       imageUrl: 'assets/cruz.jpg',
-      sizes: ['Small', 'Medium', 'Large'],
+      sizes: ['unidad', 'metro', 'paleta'],
     ),
     Product(
       name: 'Producto 2',
@@ -31,20 +31,71 @@ class Catalogo extends StatelessWidget {
     // Agrega más productos según sea necesario
   ];
 
-  Catalogo({super.key});
+  Catalogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 16,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catálogo de Productos'),
       ),
-      itemCount: products.length,
-      itemBuilder: (context, index) {
-        return ProductCard(product: products[index]);
-      },
+      body: Container(
+        color: Colors.white, // Fondo blanco
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+          ),
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            return ProductCard(product: products[index]);
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white, // Fondo blanco
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: ProductSearchDelegate(products),
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {
+                // Acción al presionar el botón de carrito de compras
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () {
+                // Acción al presionar el botón de perfil de usuario
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Acción al presionar el botón flotante
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
@@ -72,6 +123,7 @@ class ProductCard extends StatelessWidget {
               height: 150,
             ),
           ),
+          SizedBox(height: 8), // Añade un pequeño espacio
           Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
@@ -80,7 +132,9 @@ class ProductCard extends StatelessWidget {
                 Text(
                   product.name,
                   style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 const SizedBox(height: 8),
                 Text(
@@ -93,23 +147,136 @@ class ProductCard extends StatelessWidget {
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 8),
-                Wrap(
-                  spacing: 8,
-                  children: product.sizes
-                      .map(
-                        (size) => Chip(
-                          label: Text(size),
-                          backgroundColor: Colors.blue,
-                          labelStyle: const TextStyle(color: Colors.white),
-                        ),
-                      )
-                      .toList(),
+                Container(
+                  margin: const EdgeInsets.only(top: 8),
+                  child: Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: product.sizes
+                        .map(
+                          (size) => Chip(
+                            label: Text(size),
+                            backgroundColor: Colors.blue,
+                            labelStyle: const TextStyle(color: Colors.white),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SmallButton(
+                      label: 'Button 1',
+                      onPressed: () {
+                        // Acción al presionar el botón 1
+                      },
+                    ),
+                    SmallButton(
+                      label: 'Button 2',
+                      onPressed: () {
+                        // Acción al presionar el botón 2
+                      },
+                    ),
+                    SmallButton(
+                      label: 'Button 3',
+                      onPressed: () {
+                        // Acción al presionar el botón 3
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class SmallButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+
+  const SmallButton({Key? key, required this.label, this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(label),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.blue,
+        textStyle: const TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
+
+class ProductSearchDelegate extends SearchDelegate<Product> {
+  final List<Product> products;
+
+  ProductSearchDelegate(this.products);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, query as Product);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final results = products.where(
+        (product) => product.name.toLowerCase().contains(query.toLowerCase()));
+
+    return ProductSearchResults(results.toList());
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = products.where(
+        (product) => product.name.toLowerCase().contains(query.toLowerCase()));
+
+    return ProductSearchResults(suggestions.toList());
+  }
+}
+
+class ProductSearchResults extends StatelessWidget {
+  final List<Product> results;
+
+  ProductSearchResults(this.results);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(results[index].name),
+          onTap: () {
+            // Acción al seleccionar un resultado de búsqueda
+          },
+        );
+      },
     );
   }
 }
