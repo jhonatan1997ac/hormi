a37872f4a1e5c13915227563b50bfc63a8418b5d
Jhonatan <jhonatan.alarcon2767@utc.edu.ec>
2024-03-09 17:30:18 -0500

    quitado los campos que no vale

--- lib/pages/inicio/bodega/proceso_producto_bodega.dart
+++ lib/pages/inicio/bodega/proceso_producto_bodega.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, unnecessary_null_comparison, unused_local_variable
+// ignore_for_file: prefer_typing_uninitialized_variables, unnecessary_null_comparison, unused_local_variable, library_private_types_in_public_api, sort_child_properties_last, non_constant_identifier_names
 
 import 'package:apphormi/pages/inicio/administrador/Agregacion/agregar_producto_administrador.dart';
 import 'package:apphormi/pages/inicio/bodega/bodeguero.dart';
